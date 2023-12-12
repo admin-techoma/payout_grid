a429d89b2b179f5d2a16d2832bfde1abf4284ad0
dvenila <venila.pspl@gmail.com>
2023-12-12 18:14:56 +0530

    rto month state  company script

--- grid/urls.py
+++ grid/urls.py
@@ -7,6 +7,12 @@ urlpatterns = [
     path('', views.view_grid, name='view_grid'),
     path('upload_grid/', views.upload_grid, name='upload_grid'),
     path('ajax/load_vehical_type/', views.load_vehical_type, name='ajax_load_vehical_type'),
+    path('ajax/load_vehical_subtype/', views.load_vehical_subtype, name='ajax_load_vehical_subtype'),
+    path('ajax/load_product_name/', views.load_product_name, name='ajax_load_product_name'),
+    path('ajax/load_company/', views.load_company, name='ajax_load_company'),
+    path('ajax/load_month/', views.load_month, name='ajax_load_month'),
+    path('ajax/load_state/', views.load_state, name='ajax_load_state'),
+    path('ajax/load_rto/', views.load_rto, name='ajax_load_rto'),
   
 ]
 
--- grid/views.py
+++ grid/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from grid.models import Product
 from decimal import Decimal
 from django.db.models import Sum
@@ -13,7 +12,7 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 import os
 from grid.models import Grid_data
-
+from django.core.serializers import serialize
 
 
 
@@ -24,24 +23,60 @@ def view_grid(request):
     }
     return render(request, 'view_grid.html', context)
 
-from django.http import JsonResponse
 
-from django.http import JsonResponse
-from django.core.serializers import serialize
 
 def load_vehical_type(request):
     product = request.GET.get('product')
     vehical_types = Grid_data.objects.filter(product=product).order_by('vehical_type').values('vehical_type').distinct()
-    
-    # Convert the QuerySet to a list of dictionaries
     vehical_types_list = list(vehical_types)
-    
-    # You can use Django's built-in serializers to serialize the data
-    # serialized_data = serialize('json', vehical_types, fields=('vehical_type',))
-    
     return JsonResponse({'vehical_types': vehical_types_list})
 
+def load_vehical_subtype(request):
+    vehical_type = request.GET.get('vehical_type')
+    vehical_subtypes = Grid_data.objects.filter(vehical_type=vehical_type).order_by('vehical_subtype').values('vehical_subtype').distinct()
+    vehical_subtypes_list = list(vehical_subtypes)
+    return JsonResponse({'vehical_subtypes': vehical_subtypes_list})
 
+def load_product_name(request):
+    vehical_subtype = request.GET.get('vehical_subtype')
+    product_names = Grid_data.objects.filter(vehical_subtype=vehical_subtype).order_by('product_name').values('product_name').distinct()
+    product_names_list = list(product_names)
+    print("product_names_list************",product_names_list)
+    return JsonResponse({'product_names': product_names_list})
+
+def load_company(request):
+    # import pdb ; pdb.set_trace()
+    product_name = request.GET.get('company')  # Correct parameter name
+    companys = Grid_data.objects.filter(product_name=product_name).order_by('company').values('company').distinct()
+    companys_list = list(companys)
+    
+    return JsonResponse({'companys': companys_list})
+
+
+
+def load_month(request):
+    # import pdb ; pdb.set_trace()
+    company = request.GET.get('month')  # Correct parameter name
+    months = Grid_data.objects.filter(company=company).order_by('month').values('month').distinct()
+    months_list = list(months)
+    
+    return JsonResponse({'months': months_list})
+
+def load_state(request):
+    # import pdb ; pdb.set_trace()
+    month = request.GET.get('state')  # Correct parameter name
+    states = Grid_data.objects.filter(month=month).order_by('state').values('state').distinct()
+    states_list = list(states)
+    
+    return JsonResponse({'states': states_list})
+
+def load_rto(request):
+    # import pdb ; pdb.set_trace()
+    state = request.GET.get('rto')  # Correct parameter name
+    rtos = Grid_data.objects.filter(state=state).order_by('rto').values('rto').distinct()
+    rtos_list = list(rtos)
+    
+    return JsonResponse({'rtos': rtos_list})
 
 
 def reco_doc_path(instance,filename):
